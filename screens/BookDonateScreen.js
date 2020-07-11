ffd67b3dc7730523153376ab24c8f240bed478df
divyavala3 <divyavala3@gmail.com>
2020-07-11 15:13:24 +0530

    c79

---
+++ screens/BookDonateScreen.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import { View, StyleSheet, Text, FlatList,TouchableOpacity } from 'react-native';
+import { ListItem } from 'react-native-elements'
+import firebase from 'firebase';
+import db from '../config'
+import MyHeader from '../components/MyHeader';
+
+export default class BookDonateScreen extends Component{
+  
+  render(){
+    return(
+      <Text>Book Donate Screen</Text>
+    )
+  }
+}
+
+
